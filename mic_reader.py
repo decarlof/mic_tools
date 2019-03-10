bfe06d88e82e2a1678a2220ebacb39f8fdb3a8b7
Francesco De Carlo <decarlof@gmail.com>
2019-03-09 20:48:53 -0600

    tested on 2-ID-E data

--- mic_reader.py
+++ mic_reader.py
@@ -9,8 +9,11 @@ from __future__ import (absolute_import, division, print_function,
 
 import os
 import sys
+import numpy as np
 import argparse
 import dxchange
+from pylab import *
+import dxfile.dxtomo as dx
 
 __author__ = "Francesco De Carlo"
 __copyright__ = "Copyright (c) 2018, UChicago Argonne, LLC."
@@ -23,12 +26,17 @@ __all__ = ['read_projection',
 
 def find_index(a_list, element):
     try:
-        return a_list.tolist().index(element)
+        return a_list.index(element)
     except ValueError:
         return None
 
 def read_elements(h5fname):
-    return(dxchange.read_hdf5(h5fname, "MAPS/channel_names"))
+    b_elements = dxchange.read_hdf5(h5fname, "MAPS/channel_names")
+    elements = []
+
+    for i, e in enumerate(b_elements):
+        elements.append(e.decode('utf-8'))
+    return(elements)
 
 def read_projection(fname, element, theta_index):
     """
@@ -57,39 +65,60 @@ def read_projection(fname, element, theta_index):
     theta = float(dxchange.read_hdf5(fname, "MAPS/extra_pvs_as_csv")[theta_index].split(b',')[1])
     elements = read_elements(fname)
 
-    return projections[find_index(elements, element)], theta
+    try:
+        find_index(elements, element)
+        return projections[find_index(elements, element),:, :], theta
+    except None:
+        print("ERROR: Element %s does exist in the file: %s " % (element, fname))
+        pass
+
+def write_dxfile(fname, proj, theta, element):
+    experimenter_affiliation="Argonne National Laboratory" 
+    instrument_name="2-ID-E XRF"  
+    sample_name = "test data set"
+
+    flat = ones([1, proj.shape[1], proj.shape[2]])
+    dark = zeros([1, proj.shape[1], proj.shape[2]])
+
+    # Open DataExchange file
+    f = dx.File(fname, mode='w')
+     
+    # Write the Data Exchange HDF5 file.
+    f.add_entry(dx.Entry.experimenter(affiliation={'value': experimenter_affiliation}))
+    f.add_entry(dx.Entry.instrument(name={'value': instrument_name}))
+    f.add_entry(dx.Entry.sample(name={'value': sample_name}))
+    f.add_entry(dx.Entry.sample(description={'value': element}))
+
+    f.add_entry(dx.Entry.data(data={'value': proj, 'units':'counts'}))
+    f.add_entry(dx.Entry.data(data_white={'value': flat, 'units':'counts'}))
+    f.add_entry(dx.Entry.data(data_dark={'value': dark, 'units':'counts'}))
+    f.add_entry(dx.Entry.data(theta={'value': theta, 'units':'degrees'}))
+
+    f.close()
 
 
-# use test data at https://anl.box.com/s/qinted32vyrcnjyt7tzs3cx6kreeud3m
-
+# tested on data from https://anl.box.com/s/qinted32vyrcnjyt7tzs3cx6kreeud3m
 def main(arg):
 
     parser = argparse.ArgumentParser()
     parser.add_argument("fname", help="Directory containing multiple datasets or file name of a single dataset: /data/ or /data/sample.h5")
-    parser.add_argument("--start", nargs='?', type=float, default=0.0, help="Angle first projection (default 0.0)")
-    parser.add_argument("--end", nargs='?', type=float, default=180.0, help="Angle last projection (default 180.0)")
-    parser.add_argument("--element", nargs='?', type=str, default="Si", help="Select the element to recontruct (default Si)")
+    parser.add_argument("--element", nargs='?', type=str, default="Ca", help="Select the element to recontruct (default Si)")
+    parser.add_argument("--output", nargs='?', type=str, default="./tmp/data", help="Output file path and prefix (default ./tmp/data)")
+    parser.add_argument("--theta_index", nargs='?', type=int, default=657, help="theta_index: 2-ID-E: 663; 2-ID-E prior 2017: 657; BNP 8; (default 657)")
 
     args = parser.parse_args()
 
-    # Set path to the micro-CT data to reconstruct.
     fname = args.fname
-    angle_start = float(args.start)
-    angle_end = float(args.end)
     element = args.element
+    out = args.output
+    theta_index = args.theta_index
 
     if os.path.isfile(fname):    
 
-        elements = mr.read_elements(fname)
-
-        for i, e in enumerate(elements):
-            print ('%d:  %s' % (i, e))
-        
-        proj, theta = mr.read_projection(fname, element, 663)
+        elements = read_elements(fname)        
+        proj, theta = read_projection(fname, element, theta_index)
         print ("theta:", theta)
-#        print (theta.shape)
-        print ("proj:", proj)
-        print (proj.shape)
+        print ("projection shape", proj.shape)
 
     elif os.path.isdir(fname):
         # Add a trailing slash if missing
@@ -97,11 +126,17 @@ def main(arg):
 
         h5_file_list = list(filter(lambda x: x.endswith(('.h5', '.hdf')), os.listdir(top)))
 
-        for i, fname in enumerate(h5_file_list):
-            proj, theta = mr.read_projection(top+fname, element, 657) ##BNP 8; 2-ID-E: 663; 2-ID-E prior 2017: 657
-            # print(i, theta, fname)
-            print(theta, fname)
+        proj, theta = read_projection(top+h5_file_list[0], element, theta_index) 
+        print("\n (element, theta.shape, proj.shape)", element, len(h5_file_list), proj.shape)
+        data = zeros([len(h5_file_list), proj.shape[0], proj.shape[1]])
+        theta = zeros([len(h5_file_list)])
 
+        for i, fname in enumerate(h5_file_list):
+            proj, theta_image = read_projection(top+fname, element, theta_index) 
+            data[i, :, :] = proj
+            theta[i] = theta_image
+            dxchange.write_tiff(proj, out+".tiff")
+        write_dxfile(out+".h5", data, theta, element)
     else:
         print("Directory or File Name does not exist: ", fname)
 
