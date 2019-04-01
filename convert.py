ecacc0e7416f73ad6782df0e826b69b11cd0ab1a
Francesco De Carlo <decarlof@gmail.com>
2019-03-31 20:07:22 -0500

    renamed convert4d to convert

--- convert.py
+++ convert.py
@@ -15,12 +15,14 @@ import dxchange
 from pylab import *
 import dxfile.dxtomo as dx
 
+from elements import ELEMENTS
+
 __author__ = "Francesco De Carlo"
 __copyright__ = "Copyright (c) 2018, UChicago Argonne, LLC."
 __version__ = "0.0.1"
 __docformat__ = 'restructuredtext en'
 __all__ = ['read_projection',
-           'read_elements',
+           'read_channel_names',
            'find_index']
 
 
@@ -30,13 +32,24 @@ def find_index(a_list, element):
     except ValueError:
         return None
 
-def read_elements(h5fname):
-    b_elements = dxchange.read_hdf5(h5fname, "MAPS/channel_names")
-    elements = []
+def find_elements(channel_names):
 
-    for i, e in enumerate(b_elements):
-        elements.append(e.decode('utf-8'))
-    return(elements)
+    elements = []
+    for i in range(1, 110, 1): 
+         elements.append(str(ELEMENTS[i].symbol))
+
+    # print(channel_names)
+    # print(sorted(set(channel_names) & set(elements), key = channel_names.index))
+    elements = sorted(set(channel_names) & set(elements), key = channel_names.index)
+
+    return elements
+
+def read_channel_names(h5fname):
+    b_channel_names = dxchange.read_hdf5(h5fname, "MAPS/channel_names")
+    channel_names = []
+    for i, e in enumerate(b_channel_names):
+        channel_names.append(e.decode('utf-8'))
+    return(channel_names)
 
 def read_projection(fname, element, theta_index):
     """
@@ -63,7 +76,7 @@ def read_projection(fname, element, theta_index):
 
     projections = dxchange.read_hdf5(fname, "MAPS/XRF_roi")
     theta = float(dxchange.read_hdf5(fname, "MAPS/extra_pvs_as_csv")[theta_index].split(b',')[1])
-    elements = read_elements(fname)
+    elements = read_channel_names(fname)
 
     try:
         if find_index(elements, element) != None:
@@ -74,7 +87,7 @@ def read_projection(fname, element, theta_index):
         print("**** ERROR: Element %s does exist in the file: %s " % (element, fname))
         return None
 
-def write_dxfile(fname, proj, theta, element):
+def write_dxfile(fname, proj, theta, elem):
     experimenter_affiliation="Argonne National Laboratory" 
     instrument_name="2-ID-E XRF"  
     sample_name = "test data set"
@@ -82,6 +95,8 @@ def write_dxfile(fname, proj, theta, element):
     flat = ones([1, proj.shape[1], proj.shape[2]]) * np.nanmax(proj)
     dark = zeros([1, proj.shape[1], proj.shape[2]])
 
+    # print(theta)
+    print(elem)
     # Open DataExchange file
     f = dx.File(fname, mode='w')
      
@@ -90,10 +105,12 @@ def write_dxfile(fname, proj, theta, element):
     f.add_entry(dx.Entry.instrument(name={'value': instrument_name}))
     f.add_entry(dx.Entry.sample(name={'value': sample_name}))
 
-    f.add_entry(dx.Entry.data(data={'value': proj, 'element': element, 'units':'counts'}))
-    f.add_entry(dx.Entry.data(data_white={'value': flat, 'units':'counts'}))
-    f.add_entry(dx.Entry.data(data_dark={'value': dark, 'units':'counts'}))
+    f.add_entry(dx.Entry.data(data={'value': proj, 'units':'counts'}))
+    # f.add_entry(dx.Entry.data(elements={'value': elements, 'units':'string'}))
     f.add_entry(dx.Entry.data(theta={'value': theta, 'units':'degrees'}))
+
+    elem = [x.encode('utf-8') for x in elem]
+    f.add_entry(dx.Entry.data(elements={'value': elem, 'units':'counts'}))
 
     f.close()
 
@@ -101,48 +118,40 @@ def write_dxfile(fname, proj, theta, element):
 def main(arg):
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("fname", help="directory containing multiple datasets or file name of a single dataset: /data/ or /data/sample.h5")
-    parser.add_argument("--element", nargs='?', type=str, default="Ca", help="element selection (default Si)")
-    parser.add_argument("--output_fname", nargs='?', type=str, default="./data", help="output file path and prefix (default ./tmp/data)")
-    parser.add_argument("--output_fformat", nargs='?', type=str, default="hdf", help="output file format: hdf or tiff (default hdf)")
+    parser.add_argument("fname", help="directory containing multiple datasets: /data/")
+    parser.add_argument("--output_fname", nargs='?', type=str, default="./data", help="output file path and prefix (default ./data)")
     parser.add_argument("--theta_index", nargs='?', type=int, default=657, help="theta_index: 2-ID-E: 663; 2-ID-E prior 2017: 657; BNP 8; (default 657)")
 
     args = parser.parse_args()
 
     fname = args.fname
-    element = args.element
     out = args.output_fname
-    fformat = args.output_fformat
     theta_index = args.theta_index
 
-    if os.path.isfile(fname):    
-
-        proj, theta = read_projection(fname, element, theta_index)
-        print ("theta:", theta)
-        print ("projection shape", proj.shape)
-
-    elif os.path.isdir(fname):
+    if os.path.isdir(fname):
         # Add a trailing slash if missing
         top = os.path.join(fname, '')
 
         h5_file_list = list(filter(lambda x: x.endswith(('.h5', '.hdf')), os.listdir(top)))
 
-        # elements = read_elements(top+h5_file_list[0])
-        # print ("Elements in the files: ", elements)
 
-        proj, theta = read_projection(top+h5_file_list[0], element, theta_index) 
-        print("\n (element, theta.shape, proj.shape)", element, len(h5_file_list), proj.shape)
-        data = zeros([len(h5_file_list), proj.shape[0], proj.shape[1]])
+        print(top+h5_file_list[0])
+        channel_names = read_channel_names(top+h5_file_list[0])
+        print ("Channel Names: ", channel_names)
+
+        elements = find_elements(channel_names)
+
+        proj, theta = read_projection(top+h5_file_list[0], elements[0], theta_index) 
+        print("\n (element, theta.shape, proj.shape)", elements[0], len(h5_file_list), proj.shape)
+        data = zeros([len(elements), len(h5_file_list), proj.shape[0], proj.shape[1]])
         theta = zeros([len(h5_file_list)])
 
-        for i, fname in enumerate(h5_file_list):
-            proj, theta_image = read_projection(top+fname, element, theta_index) 
-            data[i, :, :] = proj
-            theta[i] = theta_image
-            if fformat == "tiff":
-                dxchange.write_tiff(proj, out + "_" + element + ".tiff")
-        if fformat == "hdf":
-            write_dxfile(out + "_" + element + ".h5", data, theta, element)
+        for j, element in enumerate(elements):
+            for i, fname in enumerate(h5_file_list):
+                proj, theta_image = read_projection(top+fname, element, theta_index) 
+                data[j, i, :, :] = proj
+                theta[i] = theta_image
+        write_dxfile(out + ".h5", data, theta, elements)
     else:
         print("Directory or File Name does not exist: ", fname)
 
